df3efb46345bca0bf2e4736c8848523fdfe21dc2
Natan Schneider <natanschneider@protonmail.com>
2023-04-07 16:45:47 -0300

    *fix* file indentation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html", "./src/**/*.{sxs,js}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ["inter", "serif"],
-      }
-    },
-  },
-  plugins: [],
+	content: ["index.html", "./src/**/*.{sxs,js}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ["inter", "serif"],
+			}
+		},
+	},
+	plugins: [],
 }
 
