4321dcfbfbd4fa21576979eb9f7721beb142a716
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 18:48:34 -0300

    *feature* add styling and iterate through projects array

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-export function Portfolio() {
+import React from 'react';
+import projects from '../data/projects';
+import PortfolioItem from './PortfolioItem';
+
+function Portfolio() {
 	return (
-		<div>Portfolio</div>
+		<div className="flex flex-col md:flex-row items-center justify-center">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+				{projects.map(project => (
+					<PortfolioItem
+						imgUrl={project.imgUrl}
+						title={project.title}
+						stack={project.stack}
+						link={project.link}
+					/>
+				))}
+			</div>
+		</div>
 	)
 }
 
