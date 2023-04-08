2f1ea02d692e132ae9194e190f0475adac16abbf
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 16:53:37 -0300

    *feature* content and styling to intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 export function Intro() {
 	return (
-		<div>Intro</div>
+		<div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+			<h1 className="text-4x1 md:text-7x1 mb-1 md:mb-3 font-bold">Natãn Schneider</h1>
+			<p className="text-base md:text-x1 mb-3 font-medium">
+				Desenvolvedor de Software. Situado no Brasil
+			</p>
+			<p className="text-small max-w-x1 mb-6 font-bold">
+				Sou uma pessoa com muita sede de conhecimento. Ótimo em aprender sozinho, e em trabalho em equipe. <br />
+				Comecei meu aprendizado em 2020, e foi aonde me encontrei como pessoa e como profissional.
+			</p>
+		</div>
 	)
 }
 
