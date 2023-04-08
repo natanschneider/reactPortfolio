c6a2c9c39373396e9b933681ae3fee5d8242a042
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 17:48:07 -0300

    *feature* Array with project details

---
+++ src/data/projects.js
@@ -0,0 +1,24 @@
+export default [
+	{
+		title: 'Pomodorish',
+		imgURL: 'TO-DO',
+		stack: [
+			'JavaScript',
+			'CSS'
+		],
+		link: 'https://pomodorish.netlify.app/',
+		repository: 'https://github.com/natanschneider/pomodorish'
+	},
+	{
+		title: 'Campeonato Brasileiro',
+		imgURL: 'TO-DO',
+		stack: [
+			'PHP',
+			'CSS',
+			'MariaDB',
+			'API'
+		],
+		link: 'https://tabelabrasileiroseriea.000webhostapp.com/',
+		repository: 'https://github.com/natanschneider/TabelaBrasileirao'
+	},
+]
