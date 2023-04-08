3cd68b4d931a9ef5b30c155d8eafe21f4edf9ce6
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 19:34:16 -0300

    *feature* array with timeline details

---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: '2023',
+        title: 'Desenvolvedor FullStack @ Deltasul Utilidades',
+        duration: 'Abril/2023 - o momento',
+        details: '',
+    },
+    {
+        year: '2023',
+        title: 'Instrutor de Desenvolvimento Web e Informatica Fundamental @ Senac Santa Cruz do Sul',
+        duration: 'Janeiro/2023 - Março/2023',
+        details: '',
+    },
+    {
+        year: '2020',
+        title: 'Curso Técnico em Informatica @ Senac Santa Cruz do Sul',
+        duration: 'Março/2020 - Outubro/2022',
+        details: '',
+    },
+]
