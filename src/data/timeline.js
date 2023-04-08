f58fbb31511baf35e80e057e94075c419e5e4c3c
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 19:51:07 -0300

    *feature* add details to timeline array

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,18 +3,21 @@ export default [
         year: '2023',
         title: 'Desenvolvedor FullStack @ Deltasul Utilidades',
         duration: 'Abril/2023 - o momento',
-        details: '',
+        details:
+            'Sou responsável pelo desenvolvimento e manutenção de sistemas Web internos, utilizando PHP, JavaScript e ORACLE SQL.',
     },
     {
         year: '2023',
         title: 'Instrutor de Desenvolvimento Web e Informatica Fundamental @ Senac Santa Cruz do Sul',
         duration: 'Janeiro/2023 - Março/2023',
-        details: '',
+        details:
+            'Ensinei jovens de 14 a 19 anos sobre Desenvolvimento Web utilizando PHP, Javascript, Node.JS, Express e bancos de dados como MariaDB e MongoDB.',
     },
     {
         year: '2020',
         title: 'Curso Técnico em Informatica @ Senac Santa Cruz do Sul',
         duration: 'Março/2020 - Outubro/2022',
-        details: '',
+        details:
+            'Curso aonde aprendi conceitos e pratica sobre Hardware e Software, escrevi minhas primeiras linhas de código e me apaixonei pela area de Desenvolvimento!',
     },
 ]
