ee23de44848978574ae27ddc4aa23054089a858f
Natan Schneider <natanschneider@protonmail.com>
2023-04-08 19:44:39 -0300

    *feature* add styling and iterate through timeline array

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-export function Timeline() {
+import timeline from "../data/timeline";
+import TimelineItem from "./TimelineItem.jsx";
+
+function Timeline() {
 	return (
-		<div>Timeline</div>
+		<div className="flex flex-col md:flex-row justify-center my-20">
+			<div className="w-full md:w-7/12">
+				{timeline.map(item => (
+					<TimelineItem
+						year={item.year}
+						title={item.title}
+						duration={item.duration}
+						details={item.details}
+					/>
+				))}
+			</div>
+		</div>
 	)
 }
 
