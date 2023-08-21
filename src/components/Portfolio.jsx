e81fa900dcd359d8c810be0b47f9f6f9d5eee6e6
natanschneider <natanschneider@protonmail.com>
2023-08-21 15:58:56 -0300

    *feat* portfolio axios call

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import projects from '../data/projects';
 import PortfolioItem from './PortfolioItem';
+import axios from 'axios';
 
 function Portfolio() {
+	const [portfolio, setPortfolio] = useState([])
+
+	useEffect(() => {
+		fetchPortfolio();
+	}, []);
+
+	const fetchPortfolio = () => {
+		axios({
+			method: 'get',
+			url: import.meta.env.VITE_API_URL+'/portfolio',
+			data: {
+				token: import.meta.env.VITE_API_TOKEN
+			},
+			headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin' : '*',
+					'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+			}
+		})
+		.then(response => {
+			console.log(response.data)
+			setPortfolio(response.data)
+		})
+		.catch(error => {
+			console.log(error.message)
+		})
+	}
+
 	return (
 		<div className="flex flex-col md:flex-row items-center justify-center">
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
