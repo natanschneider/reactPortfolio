53e382755cf3a2cd4b9a6fd4e2457c0728974fc3
natanschneider <natanschneider@protonmail.com>
2023-08-25 16:11:10 -0300

    *feat* Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {useState, useEffect} from 'react';
+import { useParams } from "react-router-dom";
+
+export default function Post(){
+    let {id} = useParams()
+    const [Post, setPost] = useState([]);
+
+    useEffect(() => {
+        fetchPost();
+    }, [])
+    
+    const fetchPost = async () => {
+        try{
+            const reqBody = {
+                id: id
+            }
+
+            const response = await axios.get(import.meta.env.VITE_API_URL + '/post', {
+                params: {
+                    id: id
+                }
+            });
+            setPost(response.data);
+        }catch(e){
+            console.log('ERRO AXIOS POST: ' + e);
+        }
+    }
+
+    const component = (
+        <>
+            
+                <div>
+                    <h1>{Post.title}</h1>
+
+                    <p>{Post.content}</p>
+                </div>
+          
+        </>
+    );
+
+    return component;
+}
