ea2434edb7c18de9b6f594bd7f4b9d75b926888e
natanschneider <natanschneider@protonmail.com>
2023-08-29 17:35:36 -0300

    *feat* Posts component

---
+++ src/components/Posts.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import PostsItem from "./PostsItem";
+
+export default function Posts(){
+    const [feed, setFeed] = useState([]);
+
+    useEffect(() => {
+        fetchFeed();
+    }, [])
+    
+    const fetchFeed = async () => {
+        try{
+            const response = await axios.get(import.meta.env.VITE_API_URL + '/feed');
+            setFeed(response.data);
+        }catch(e){
+            console.log(e.message);
+        }
+    }
+
+    const component = (
+        <div>
+            <Header />
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+                {feed.map(row => (
+                    <li 
+                        className="mb-10 ml-4" 
+                        key={row.id}
+                    >
+                        <PostsItem
+                            id={row.id}
+                            title={row.title}
+                            content={row.content}
+                        />
+                    </li>
+                ))}
+            </ol>
+            <Footer />
+        </div>
+    );
+
+    return(component);
+}
