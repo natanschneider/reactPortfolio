4e71e4c32aafadfee38e07669142ffb274a11633
natanschneider <natanschneider@protonmail.com>
2023-09-06 11:09:12 -0300

    *fix* Fix Router tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import Intro from './components/Intro';
-import Portfolio from './components/Portfolio';
-import Timeline from './components/Timeline';
-import Feed from './components/Feed';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 import Post from './components/Post'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Posts from './components/Posts';
-import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
-
-
-
 	return (
-		
-
 			<div className="bg-white dark:bg-gray-900 text-stone-900 dark:text-stone-300 min-h-screen font-inter">
 				<div className="max-w-5xl w-11/12 mx-auto">
 					<Router>
 						<Routes>
-							<Route path='/' exact Component={Home} />
-							<Route path='/post/:id' exact Component={Post} />
-							<Route path='/post' exact Component={Posts} />
-							<Route render={() => <h1>404: page not found</h1>} />
+							<Route path='/' element={ <Home /> } exact />
+							<Route path='/post/:id' element={ <Post /> } exact />
+							<Route path='/post' element={ <Posts /> } exact />
 						</Routes>
 					</Router>
 				</div>
 			</div>
-		
 	)
 }
-
-const Home = () => (
-	<>
-		<Header />
-		<Intro />
-		<Portfolio />
-		<Timeline />
-		<Feed />
-		<Contact />
-		<Footer />
-	</>
-);
 
 export default App
