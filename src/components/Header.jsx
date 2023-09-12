3bbf07e5aae75300670217a6955ff517dc9fbb95
natanschneider <natanschneider@protonmail.com>
2023-09-12 10:21:07 -0300

    *fix* change theme switcher location

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,17 +71,15 @@ export default function Header(){
                     <li>
                         <a href="/post" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Publicações</a>
                     </li>
-					<li>
-                    <button
-                        type="button"
-                        onClick={handleThemeSwitch}
-                        className="p-1  bg-violet-300 dark:bg-orange-300 text-lg rounded-md dark:text-gray-700"
-                    >
-                        {theme === 'dark' ? sun : moon}
-			        </button>
-					</li>
 					</ul>
                 </div>
+				<button
+                    type="button"
+                    onClick={handleThemeSwitch}
+                    className="p-1  bg-violet-300 dark:bg-orange-300 text-lg rounded-md dark:text-gray-700"
+                >
+                    {theme === 'dark' ? sun : moon}
+			    </button>
             </div>
         </nav>
     );
