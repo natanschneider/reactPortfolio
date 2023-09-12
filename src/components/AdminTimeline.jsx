def2054eee95d5dcb025cfa448241621f553178f
natanschneider <natanschneider@protonmail.com>
2023-09-12 10:21:56 -0300

    *feat* AdminTimeline component

---
+++ src/components/AdminTimeline.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdminSidebar from "./AdminSidebar";
+
+export default function AdminTimeline(){
+    return(
+        <>
+            <AdminSidebar />
+            <div className="w-full">
+                <h1>Timeline</h1>
+                
+            </div>
+        </>
+    )
+}
